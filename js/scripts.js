2b1cee04f453884b469f215d28dd0a9d9c14f926
gabrielebottari <gabriele.bottari1@gmail.com>
2023-12-18 13:59:04 +0100

    ADD milestone 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,52 @@
-// Test funzionamento
-console.log('Template HTML CSS JS');
+/*
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+MILESTONE 0:
+Creare l'array di oggetti con le informazioni fornite.
+
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+*/
+
+//MILESTONE 0: Creo l'array di oggetti con le informazioni fornite
+const teamMembers = [
+    {
+      name: "Wayne Barnett",
+      role: "Founder & CEO",
+      image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+      name: "Angela Caroll",
+      role: "Chief Editor",
+      image: "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+      name: "Walter Gordon",
+      role: "Office Manager",
+      image: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+      name: "Angela Lopez",
+      role: "Social Media Manager",
+      image: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+      name: "Scott Estrada",
+      role: "Developer",
+      image: "scott-estrada-developer.jpg"
+    },
+
+    {
+      name: "Barbara Ramos",
+      role: "Graphic Designer",
+      image: "barbara-ramos-graphic-designer.jpg"
+    }
+  ];
